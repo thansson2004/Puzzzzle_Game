ea0413ac3cae4abccd0f669bfaf514523d8e9070
thansson2004 <79506289+thansson2004@users.noreply.github.com>
2021-11-26 13:56:05 +0100

    MainMenu

---
+++ puzzleGame/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public void level1()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void level2()
+    {
+        SceneManager.LoadScene(2);
+    }
+   public void level3()
+    {
+        SceneManager.LoadScene(3);
+    }
+   public void level4()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void level5()
+    {
+        SceneManager.LoadScene(5);
+    }
+   public void Meny()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
